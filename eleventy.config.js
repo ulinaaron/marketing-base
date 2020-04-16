51eeae1b557d0bb647c42140d6b158c8c7d7ba19
Aaron Mazade <aaron@stevensinc.com>
2020-04-16 16:02:28 -0400

    add filex to correct folder

--- eleventy.config.js
+++ eleventy.config.js
@@ -68,6 +68,7 @@ module.exports = eleventyConfig => {
     eleventyConfig.addPassthroughCopy("static/js")
     eleventyConfig.addPassthroughCopy("static/images")
     eleventyConfig.addPassthroughCopy("static/robots.txt")
+    eleventyConfig.addPassthroughCopy("static/_redirects")
 
     return {
         templateFormats: ["md", "njk"],
